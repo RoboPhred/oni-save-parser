3e7796cb75bca330097e670940661fff694723e9
RoboPhred <robophred@gmail.com>
2018-04-21 15:53:17 -0700

    Play with normalization

--- src/test/index.ts
+++ src/test/index.ts
@@ -6,10 +6,14 @@ import {
 
 import {
     parseSaveGame,
-    writeSaveGame
+    writeSaveGame,
+    GameObject,
+    getBehavior,
+    KPrefabIDBehavior,
+    GameObjectBehavior
 } from "../index";
 
-const fileName = "broken-before-writeback"; //"TheLoneSurvivor";
+const fileName = "TheLOSTCity"; //"TheLoneSurvivor";
 const fileData = readFileSync(`./test-data/${fileName}.sav`);
 
 console.log("Loading save");
@@ -55,5 +59,71 @@ function testDumpJson() {
     console.log("json dump completed");
 }
 
-//testDumpJson();
-testWriteback();
+// import { schema, normalize } from "normalizr";
+// function testNormalize() {
+//     const behaviorIDMap = new Map<string, number>();
+//     const streamData = saveData.body.saveRoot.streamed;
+//     for (let key of Object.keys(streamData)) {
+//         streamData[key] = "<stream data>" as any;
+//     }
+
+//     const gameObjectBehaviorSchema = new schema.Entity(
+//         "gameObjectBehavior",
+//         {},
+//         {
+//             idAttribute: (input: any, parent: any, key) => {
+//                 // Need to track the discovery offset, as
+//                 //  the array can contain multiple of the same behavior,
+//                 //  which must be kept as appropriate. 
+//                 const rootID = `${parent.type}-${parent.id}-${input.name}`;
+//                 const currentIndex = behaviorIDMap.get(rootID) || 0;
+//                 const id = rootID + '-' + currentIndex;
+//                 behaviorIDMap.set(rootID, currentIndex + 1);
+//                 return id;
+//             }
+//         }
+//     )
+//     const gameObjectBehaviorsSchema = new schema.Array(gameObjectBehaviorSchema);
+//     const gameObjectSchema = new schema.Entity(
+//         "gameObject",
+//         {
+//             behaviors: gameObjectBehaviorsSchema
+//         },
+//         {
+//             idAttribute: (input: GameObject, parent, key) => {
+//                 const idPrefab = getBehavior(input, KPrefabIDBehavior);
+//                 if (!idPrefab) return "INVALID";
+//                 return String(idPrefab.parsedData.InstanceID);
+//             },
+//             processStrategy: (input: GameObject, parent, key) => {
+//                 const idPrefab = getBehavior(input, KPrefabIDBehavior);
+//                 let id: string;
+//                 if (!idPrefab) id = "INVALID";
+//                 else id = String(idPrefab.parsedData.InstanceID);
+//                 return {
+//                     ...input,
+//                     id,
+//                     type: key
+//                 };
+//             }
+//         }
+//     );
+//     const gameObjectsSchema = new schema.Array(gameObjectSchema);
+//     const gameObjectTypesSchema = new schema.Values(
+//         gameObjectsSchema
+//     );
+//     const saveDataSchema = new schema.Object(
+//         {
+//             body: {
+//                 gameObjects: gameObjectTypesSchema
+//             }
+//         }
+//     );
+//     const data = normalize(saveData, saveDataSchema);
+//     writeFileSync(`${fileName}-normalized.json`, JSON.stringify(data, null, 2));
+// }
+
+testDumpJson();
+//testWriteback();
+
+// testNormalize();
