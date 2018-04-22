888844420aa9f27751e019cdb425ca0890e314d2
RoboPhred <robophred@gmail.com>
2018-04-22 10:28:40 -0700

    Geyser types

--- dts/versions/7_3/behaviors/geyser.d.ts
+++ dts/versions/7_3/behaviors/geyser.d.ts
@@ -16,3 +16,28 @@ export interface GeyserBehavior extends GameObjectBehavior {
         };
     };
 }
+export declare const GEYSER_TYPE_HASHES: {
+    "steam": number;
+    "hot_steam": number;
+    "hot_water": number;
+    "slush_water": number;
+    "filthy_water": number;
+    "small_volcano": number;
+    "big_volcano": number;
+    "liquid_co2": number;
+    "hot_co2": number;
+    "hot_hydrogen": number;
+    "hot_po2": number;
+    "slimy_po2": number;
+    "chlorine_gas": number;
+    "methane": number;
+    "molten_copper": number;
+    "molten_iron": number;
+    "molten_gold": number;
+    "oil_drip": number;
+};
+export declare type GeyserTypeName = keyof typeof GEYSER_TYPE_HASHES;
+export declare const GEYSER_TYPE_NAMES: ("steam" | "hot_steam" | "hot_water" | "slush_water" | "filthy_water" | "small_volcano" | "big_volcano" | "liquid_co2" | "hot_co2" | "hot_hydrogen" | "hot_po2" | "slimy_po2" | "chlorine_gas" | "methane" | "molten_copper" | "molten_iron" | "molten_gold" | "oil_drip")[];
+export declare const GEYSER_TYPE_HASH_NAMES: {
+    [key: number]: GeyserTypeName;
+};
--- src/versions/7_3/behaviors/geyser.ts
+++ src/versions/7_3/behaviors/geyser.ts
@@ -2,6 +2,8 @@
 import { GameObjectBehavior } from "../interfaces";
 import { BehaviorName } from "./interfaces";
 
+import { getSDBMHash32 } from "../helpers";
+
 export const GeyserBehavior: BehaviorName<GeyserBehavior> = "Geyser";
 export interface GeyserBehavior extends GameObjectBehavior {
     name: "Geyser";
@@ -17,4 +19,59 @@ export interface GeyserBehavior extends GameObjectBehavior {
             yearPercentRoll: number
         }
     }
+};
+
+// We want to export as number, but compute them by key.
+//  Could probably type this better by passing array to a builder func.
+export const GEYSER_TYPE_HASHES = {
+    "steam": (null as any) as number,
+    "hot_steam": (null as any) as number,
+    "hot_water": (null as any) as number,
+    "slush_water": (null as any) as number,
+    "filthy_water": (null as any) as number,
+
+    "small_volcano": (null as any) as number,
+    "big_volcano": (null as any) as number,
+    
+    "liquid_co2": (null as any) as number,
+    "hot_co2": (null as any) as number,
+
+    "hot_hydrogen": (null as any) as number,
+
+    "hot_po2": (null as any) as number,
+    "slimy_po2": (null as any) as number,
+
+    "chlorine_gas": (null as any) as number,
+
+    "methane": (null as any) as number,
+
+    "molten_copper": (null as any) as number,
+    "molten_iron": (null as any) as number,
+    "molten_gold": (null as any) as number,
+
+    "oil_drip": (null as any) as number
+};
+
+export type GeyserTypeName = keyof typeof GEYSER_TYPE_HASHES;
+export const GEYSER_TYPE_NAMES = Object.keys(GEYSER_TYPE_HASHES) as (GeyserTypeName)[];
+export const GEYSER_TYPE_HASH_NAMES: {[key: number]: GeyserTypeName} = {};
+
+for (let geyserType of GEYSER_TYPE_NAMES) {
+    const hash = getSDBMHash32(geyserType);
+    GEYSER_TYPE_HASHES[geyserType] = hash;
+    GEYSER_TYPE_HASH_NAMES[hash] = geyserType;
+}
+
+function getGeyserTypeName(hash: number): string | undefined {
+    if (!Object.prototype.hasOwnProperty.call(GEYSER_TYPE_HASH_NAMES, hash)) {
+        return undefined;
+    }
+    return GEYSER_TYPE_HASH_NAMES[hash];
+}
+
+function getGeyserTypeHash(name: string): number | undefined {
+    if (!Object.prototype.hasOwnProperty.call(GEYSER_TYPE_HASHES, name)) {
+        return undefined;
+    }
+    return GEYSER_TYPE_HASHES[name as GeyserTypeName];
 }
