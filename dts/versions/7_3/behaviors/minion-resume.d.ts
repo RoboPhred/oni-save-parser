abc2d152be4b5b3ca9235c2532cc12f61551004e
RoboPhred <robophred@gmail.com>
2018-04-23 19:44:49 -0700

    Add resume aptitude names and hashes.

--- dts/versions/7_3/behaviors/minion-resume.d.ts
+++ dts/versions/7_3/behaviors/minion-resume.d.ts
@@ -13,3 +13,23 @@ export interface MinionResumeBehavior extends GameObjectBehavior {
         targetRole?: string;
     };
 }
+export declare const APTITUDE_NAME_HASHES: {
+    "Farming": number;
+    "Ranching": number;
+    "Mining": number;
+    "Cooking": number;
+    "Art": number;
+    "Building": number;
+    "Management": number;
+    "Research": number;
+    "Suits": number;
+    "Hauling": number;
+    "Technicals": number;
+    "MedicalAid": number;
+    "Basekeeping": number;
+};
+export declare type AptitudeName = keyof typeof APTITUDE_NAME_HASHES;
+export declare const APTITUDE_NAMES: ("MedicalAid" | "Farming" | "Ranching" | "Mining" | "Cooking" | "Art" | "Building" | "Management" | "Research" | "Suits" | "Hauling" | "Technicals" | "Basekeeping")[];
+export declare const APTITUDE_HASH_NAMES: {
+    [key: number]: AptitudeName;
+};
