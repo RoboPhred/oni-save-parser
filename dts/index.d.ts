de82604b5758aced059c5c241297593e3a15ba52
RoboPhred <robophred@gmail.com>
2018-05-05 10:26:00 -0700

    Export logging and parse step services

--- dts/index.d.ts
+++ dts/index.d.ts
@@ -1,2 +1,5 @@
 export * from "./interfaces";
+export * from "./logging";
+export { createModule as createLogModule } from "./logging/module";
+export * from "./parse-steps";
 export * from "./versions/7_3";
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 
 export * from "./interfaces";
 
+export * from "./logging";
+export { createModule as createLogModule } from "./logging/module";
+
+export * from "./parse-steps";
+
 // Export current version as top level.
 export * from "./versions/7_3";
