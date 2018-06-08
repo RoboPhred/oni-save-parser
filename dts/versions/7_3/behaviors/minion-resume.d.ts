6296e75e308a7c2fa60869da8bc1c04a39104ce0
RoboPhred <robophred@gmail.com>
2018-06-07 19:46:49 -0700

    Fix sourcemaps

--- dts/versions/7_3/behaviors/minion-resume.d.ts
+++ dts/versions/7_3/behaviors/minion-resume.d.ts
@@ -14,19 +14,19 @@ export interface MinionResumeBehavior extends GameObjectBehavior {
     };
 }
 export declare const APTITUDE_NAME_HASHES: {
-    "Farming": number;
-    "Ranching": number;
-    "Mining": number;
-    "Cooking": number;
-    "Art": number;
-    "Building": number;
-    "Management": number;
-    "Research": number;
-    "Suits": number;
-    "Hauling": number;
-    "Technicals": number;
-    "MedicalAid": number;
-    "Basekeeping": number;
+    Farming: number;
+    Ranching: number;
+    Mining: number;
+    Cooking: number;
+    Art: number;
+    Building: number;
+    Management: number;
+    Research: number;
+    Suits: number;
+    Hauling: number;
+    Technicals: number;
+    MedicalAid: number;
+    Basekeeping: number;
 };
 export declare type AptitudeName = keyof typeof APTITUDE_NAME_HASHES;
 export declare const APTITUDE_NAMES: ("MedicalAid" | "Farming" | "Ranching" | "Mining" | "Cooking" | "Art" | "Building" | "Management" | "Research" | "Suits" | "Hauling" | "Technicals" | "Basekeeping")[];
