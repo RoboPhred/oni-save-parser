b1c1a70256429f22e64853ae35a4bc41041398ba
RoboPhred <robophred@gmail.com>
2018-06-13 18:40:32 -0700

    Type writing.  Untested

--- src/parse-context.ts
+++ src/parse-context.ts
@@ -1,4 +1,8 @@
-import { TemplateParser } from "./save-parser/templates/type-parser";
+import {
+  TemplateParser,
+  TemplateWriter
+} from "./save-parser/templates/type-parser";
 import { SaveGameHeader } from "./save-structure";
 
 export type ParseContext = TemplateParser & SaveGameHeader;
+export type WriteContext = TemplateWriter & SaveGameHeader;
