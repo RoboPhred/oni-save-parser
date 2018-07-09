43474d6194ced5482bdf250ed94e40055d54e7ee
RoboPhred <robophred@gmail.com>
2018-07-08 17:16:57 -0700

    Rebuild

--- dts/save-structure/const-data/roles/role-group.d.ts
+++ dts/save-structure/const-data/roles/role-group.d.ts
@@ -1,18 +1,4 @@
 import { HashedString } from "../../data-types";
-export declare const MinionRoleNames: {
-    Farming: "Farming";
-    Ranching: "Ranching";
-    Mining: "Mining";
-    Cooking: "Cooking";
-    Art: "Art";
-    Building: "Building";
-    Management: "Management";
-    Research: "Research";
-    Suits: "Suits";
-    Hauling: "Hauling";
-    Technicals: "Technicals";
-    MedicalAid: "MedicalAid";
-    Basekeeping: "Basekeeping";
-};
+export declare const MinionRoleNames: ("MedicalAid" | "Farming" | "Ranching" | "Mining" | "Cooking" | "Art" | "Building" | "Management" | "Research" | "Suits" | "Hauling" | "Technicals" | "Basekeeping")[];
 export declare type MinionRole = HashedString;
 export declare const MinionRoles: import("../../../../../../RoboPhred/oni-save-parser/src/save-structure/data-types/hashed-string").HashedStringEnum<"MedicalAid" | "Farming" | "Ranching" | "Mining" | "Cooking" | "Art" | "Building" | "Management" | "Research" | "Suits" | "Hauling" | "Technicals" | "Basekeeping">;
