9657e8da177fe2dc674f766c84fb5d2b8edccfb6
RoboPhred <robophred@gmail.com>
2018-07-12 06:57:45 -0700

    Recover lost MinionModifiersBehavior code

---
+++ src/save-structure/game-objects/game-object-behavior/known-behaviors/minion-modifiers/parser.ts
@@ -0,0 +1,131 @@
+import { validateDotNetIdentifierName } from "../../../../../utils";
+
+import {
+  ParseIterator,
+  readInt32,
+  UnparseIterator,
+  readKleiString,
+  getReaderPosition,
+  writeInt32,
+  writeKleiString,
+  writeDataLengthBegin,
+  writeDataLengthEnd
+} from "../../../../../parser";
+
+import {
+  TemplateParser,
+  TemplateUnparser
+} from "../../../../type-templates/template-data-parser";
+
+import {
+  MinionModifiersExtraData,
+  AmountInstance,
+  DiseaseInstance,
+  ModificationInstance
+} from "./minion-modifiers";
+
+export function* parseMinionModifiersExtraData(
+  templateParser: TemplateParser
+): ParseIterator<MinionModifiersExtraData> {
+  const amounts: AmountInstance[] = yield* parseModifiers<AmountInstance>(
+    "Klei.AI.AmountInstance",
+    templateParser
+  );
+  const diseases: DiseaseInstance[] = yield* parseModifiers<DiseaseInstance>(
+    "Klei.AI.DiseaseInstance",
+    templateParser
+  );
+
+  const extraData: MinionModifiersExtraData = {
+    amounts,
+    diseases
+  };
+  return extraData;
+}
+
+export function* unparseMinionModifiersExtraData(
+  extraData: MinionModifiersExtraData,
+  templateUnparser: TemplateUnparser
+): UnparseIterator {
+  yield* unparseModifiers<AmountInstance>(
+    extraData.amounts,
+    "Klei.AI.AmountInstance",
+    templateUnparser
+  );
+  yield* unparseModifiers<DiseaseInstance>(
+    extraData.diseases,
+    "Klei.AI.DiseaseInstance",
+    templateUnparser
+  );
+}
+
+function* parseModifiers<T extends ModificationInstance>(
+  modifierInstanceType: string,
+  templateParser: TemplateParser
+): ParseIterator<T[]> {
+  const count = yield readInt32();
+  const items = new Array(count);
+  for (let i = 0; i < count; i++) {
+    const modifier = yield* parseModifier<T>(
+      modifierInstanceType,
+      templateParser
+    );
+    items[i] = modifier;
+  }
+  return items;
+}
+
+function* unparseModifiers<T extends ModificationInstance>(
+  instances: T[],
+  modifierInstanceType: string,
+  templateUnparser: TemplateUnparser
+): UnparseIterator {
+  yield writeInt32(instances.length);
+  for (const instance of instances) {
+    yield* unparseModifier<T>(instance, modifierInstanceType, templateUnparser);
+  }
+}
+
+function* parseModifier<T extends ModificationInstance>(
+  modifierInstanceType: string,
+  templateParser: TemplateParser
+): ParseIterator<T> {
+  const name = yield readKleiString();
+  validateDotNetIdentifierName(name);
+  const dataLength = yield readInt32();
+
+  const startPos = yield getReaderPosition();
+  const data = yield* templateParser.parseByTemplate(modifierInstanceType);
+  const endPos = yield getReaderPosition();
+
+  const dataRemaining = dataLength - (endPos - startPos);
+  if (dataRemaining !== 0) {
+    throw new Error(
+      `Modifier "${name}" deserialized ${Math.abs(dataRemaining)} ${
+        dataRemaining > 0 ? "less" : "more"
+      } bytes type data than expected.`
+    );
+  }
+
+  const instance: ModificationInstance = {
+    name,
+    data: data
+  };
+
+  return instance as T;
+}
+
+function* unparseModifier<T extends ModificationInstance>(
+  instance: T,
+  modifierInstanceType: string,
+  templateUnparser: TemplateUnparser
+) {
+  yield writeKleiString(instance.name);
+
+  const token = yield writeDataLengthBegin();
+  yield* templateUnparser.unparseByTemplate(
+    instance.data,
+    modifierInstanceType
+  );
+  yield writeDataLengthEnd(token);
+}
