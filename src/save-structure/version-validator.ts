e7a2ced2eb66f8e4ebb1f33951810d3a1baf5716
RoboPhred <robophred@gmail.com>
2021-07-17 17:32:55 -0700

    Bump version lock to 7.25

--- src/save-structure/version-validator.ts
+++ src/save-structure/version-validator.ts
@@ -1,5 +1,5 @@
 export const CURRENT_VERSION_MAJOR = 7;
-export const CURRENT_VERSION_MINOR = [17, 23];
+export const CURRENT_VERSION_MINOR = [25];
 
 export function validateVersion(
   major: number,
